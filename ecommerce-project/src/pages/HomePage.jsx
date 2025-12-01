14f71c447b11b4be20bc22a82e328f196c1459a6
sriramasp05 <sriramasp05@gmail.com>
2025-12-01 18:01:47 +0530

    fetch data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -3,6 +3,13 @@ import { Header } from "../components/Header";
 import { products } from "../../starting-code/data/products";
 
 export function HomePage() {
+  fetch('http://localhost:3000/api/products')
+    .then((response) => {
+      return response.json()
+    }).then((data) => {
+      console.log(data)
+    })
+
   return (
     <>
       <title>Ecommerce Project</title>
