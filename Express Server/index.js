3520b4e4738923ace5cc6594cc20a35135571c3a
sriramasp05 <sriramasp05@gmail.com>
2025-12-09 21:50:02 +0530

    learning postman and http requests

--- Express Server/index.js
+++ Express Server/index.js
@@ -3,13 +3,31 @@
 //3.create index.js
 //4. npm install express
 //5. write server application in index.js
-//6.start server
+//6.start server using command node index.js
 
 //netstat -ano | findstr "LISTENING"    this is for showing all the ports that are currently running on my computer (local host)
+
+//use nodemon index.js to run the server for every change u make automatically
 
 import express from "express";
 const app = express();
 const port = 3000
+
+app.get("/", (req, res) => {
+    res.send("<h1>HEllo</h1>");
+});
+
+app.post("/register", (req, res) => {
+    res.sendStatus(201);
+});
+
+app.put("/user/sriram", (req, res) => {
+    res.sendStatus(200);
+});
+
+app.patch("/user/sriram", (req, res) => {
+    res.sendStatus(200);
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
